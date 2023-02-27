845a1dead1885af92a915b14ac8b7620b013718a
KiyaTilahun <kiyatilahun0@gmail.com>
2023-02-27 11:45:43 +0300

    added element scrolling sections and changedform

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-'use strict';
-const sign=document.querySelector(".signin");
-sign.addEventListener('click',function(){
-    
-    console.log("hello world");
+const allsections=document.querySelectorAll(".section");
+
+const revealsection=function(entries,observer){
+const [entry]=entries;
+if(!entry.isIntersecting) return;
+entry.target.classList.remove("section-hidden");
+observer.unobserve(entry.target);
+ console.log(entry);
+}
+const sectionobserver=new IntersectionObserver(revealsection,{
+root:null,
+threshold:0.30,
+});
+allsections.forEach(function(section){
+  sectionobserver.observe(section);
+  section.classList.add("section-hidden");
 })
+
 
